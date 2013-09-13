1fc63b34167cc68281e8db91259faef9e949bfe3
Abdullah <amjibaly@gmail.com>
2013-09-13 11:06:57 -0700

    Add service call caching with redis.

--- rest.js
+++ rest.js
@@ -1,24 +1,35 @@
-var https = require('https');
+var https = require('https'),
+    redis = require("redis"),
+    client = redis.createClient();
 
 exports.askYoda = function (req, res, next) {
-    var options = {
-        hostname: 'yoda.p.mashape.com',
-        port: 443,
-        path: '/yoda?sentence=' + encodeURIComponent(req.query.sentence),
-        method: 'GET',
-        headers: {
-            'X-Mashape-Authorization': process.env.MASHAPE_AUTH
-        }
-    };
-    https.request(options, function (response) {
-        var yodaSays = "ha: ";
-        response.on('data', function (data) {
-            if (data) yodaSays += data;
-        });
-        response.on('end', function (data) {
-            if (data) yodaSays += data;
-            req.yodaSays = yodaSays;
+    client.get(req.query.sentence, function (err, reply) {
+        if (reply) {
+            req.yodaSays = reply;
             return next();
-        });
-    }).end();
+        }
+        var options = {
+            hostname: 'yoda.p.mashape.com',
+            port: 443,
+            path: '/yoda?sentence=' + encodeURIComponent(req.query.sentence),
+            method: 'GET',
+            headers: {
+                'X-Mashape-Authorization': process.env.MASHAPE_AUTH
+            }
+        };
+        https.request(options, function (response) {
+            var yodaSays = "";
+            response.on('data', function (data) {
+                if (data) yodaSays += data;
+            });
+            response.on('end', function (data) {
+                if (data) yodaSays += data;
+                req.yodaSays = yodaSays;
+                client.set(req.query.sentence, yodaSays);
+                // cache result for 30 seconds
+                client.expire(req.query.sentence, 30);
+                return next();
+            });
+        }).end();
+    });
 };
